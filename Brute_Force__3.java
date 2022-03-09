e3d9c56796e02fe5e5450d63a53e18230192f5b8
ySeungJE <dbstmdwp98@naver.com>
2022-03-09 20:46:49 +0900

    [ ADD 🦅 ] : 0308 문제 풀이 :re

--- Brute_Force__3.java
+++ Brute_Force__3.java
@@ -1,5 +1,59 @@
 package everyday_Study;
 
-public class Brute_Force__3 {
+import java.util.*;
+import java.io.*;
 
+class Man {
+	int weight;
+	int height;
+	int number;
+
+	public Man(int w, int h) {
+		weight = w;
+		height = h;
+		number = 0;
+	}
 }
+
+class Brute_Force__3 {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		List<Man> list = new ArrayList<>();
+		int n = Integer.parseInt(br.readLine());
+
+		StringTokenizer st;
+
+		for (int i = 0; i < n; i++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			int weight = Integer.parseInt(st.nextToken());
+			int height = Integer.parseInt(st.nextToken());
+			list.add(new Man(weight, height));
+		}
+
+		getNumber(list);
+
+		for (int i = 0; i < list.size(); i++) {
+			bw.write(list.get(i).number + " ");
+		}
+		bw.flush();
+		
+	}
+
+	public static void getNumber(List<Man> list) {
+		int count;
+		for (int i = 0; i < list.size(); i++) {
+			count = 0;
+			for (int j = 0; j < list.size(); j++) {
+				if (i == j) continue;
+				if (list.get(i).height < list.get(j).height && list.get(i).weight < list.get(j).weight) {
+					count++;
+				}
+			}
+			list.get(i).number = count + 1;
+		}
+	}
+}
+
